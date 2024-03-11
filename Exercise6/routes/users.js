36d278efb1b3f502f72c1bcc92f43d0158b3cdd8
DESKTOP-T6O7O0P\gar21 <gar21@yandex.ru>
2024-03-11 22:08:40 +0500

    Добавлен каркас приложения ExpressJS

--- Exercise6/routes/users.js
+++ Exercise6/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+const users = [
+  { id: 1, name: 'Bob'},
+  { id: 2, name: 'Tom'},
+  { id: 3, name: 'Jack'}    
+];
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(users);
 });
 
 module.exports = router;
