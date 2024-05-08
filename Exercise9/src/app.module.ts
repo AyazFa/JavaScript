38b65d106160d212925ad2884c9f56b4635cabe9
DESKTOP-T6O7O0P\gar21 <gar21@yandex.ru>
2024-05-08 17:24:13 +0500

    postgres hostname for deploy

--- Exercise9/src/app.module.ts
+++ Exercise9/src/app.module.ts
@@ -11,7 +11,7 @@ import { Task } from "./tasks/entities/task.entity"
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'postgres',
+      host: 'dpg-cotm9n7109ks73anpfo0-a',
       port: 5432,
       username: 'otus',
       password: '202312',
